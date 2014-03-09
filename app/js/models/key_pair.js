b8142700dbe5c9f4031c866cd6ac2d4dba2c55ee
Mike Enriquez <mike@enriquez.me>
2014-03-09 00:27:16 -0500

    Clean up and remove stub

--- app/js/models/key_pair.js
+++ app/js/models/key_pair.js
@@ -4,29 +4,6 @@
     self = this;
 
     self.isGenerated = false;
-
-  //   self.bitcoin_worker = new Worker('/js/workers/bitcoin_worker.js');
-  //   self.bitcoin_worker.postMessage();
-  //   self.bitcoin_worker.currentMessageId = 0;
-  //   self.bitcoin_worker.callbacks = {};
-  //   self.bitcoin_worker.onmessage = function(e) {
-  //     var message = e.data;
-  //     self.bitcoin_worker.callbacks[message.id].apply(null, [message.result]);
-  //     delete self.bitcoin_worker.callbacks[message.id];
-  //   };
-
-  //   self.bitcoin_worker.async = function(functionName, params, hollaback) {
-  //     var message = {
-  //       id: self.bitcoin_worker.currentMessageId,
-  //       functionName: functionName,
-  //       params: params,
-  //     };
-
-  //     self.bitcoin_worker.callbacks[message.id] = hollaback;
-  //     self.bitcoin_worker.postMessage(message);
-
-  //     self.bitcoin_worker.currentMessageId++;
-  //   };
   }
 
   KeyPair.prototype.generate = function(password, hollaback) {
@@ -43,8 +20,7 @@
       self.encryptedPrivateKeyExponent = keyPair.encryptedPrivateKeyExponent;
       self.publicKeyX = keyPair.publicKeyX;
       self.publicKeyY = keyPair.publicKeyY;
-      // self.bitcoinAddress = keyPair.bitcoinAddress;
-      self.bitcoinAddress = "1GyqJhgVYkjBgEy8wf2iApwx4EvU3LvEtR";
+      self.bitcoinAddress = keyPair.bitcoinAddress;
 
       if (typeof hollaback === "function") {
         hollaback(keyPair);
