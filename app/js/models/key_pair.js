5fea0c1ff833a52f4340451f573e61b4e9d0f32e
Mike Enriquez <mike@enriquez.me>
2014-03-10 23:51:40 -0400

    Persist keys to browser

--- app/js/models/key_pair.js
+++ app/js/models/key_pair.js
@@ -1,9 +1,42 @@
 (function(bitcoinWorker, Models) {
 
   function KeyPair() {
-    self = this;
-    self.isGenerated = false;
+    var self = this;
+
+    if (self.restoreFromStore()) {
+      self.isGenerated = true;
+    } else {
+      self.isGenerated = false;
+    }
   }
+
+  KeyPair.prototype.saveToStore = function() {
+    var self = this;
+
+    localStorage.setItem('keyPairStore', JSON.stringify({
+      encryptedPrivateKeyExponent: self.encryptedPrivateKeyExponent,
+      publicKeyX: self.publicKeyX,
+      publicKeyY: self.publicKeyY,
+      bitcoinAddress: self.bitcoinAddress
+    }));
+  };
+
+  KeyPair.prototype.restoreFromStore = function() {
+    var self = this,
+        keyPairStore = localStorage.getItem('keyPairStore');
+
+    if (keyPairStore) {
+      var keyPairStoreData = JSON.parse(keyPairStore);
+      self.encryptedPrivateKeyExponent = keyPairStoreData.encryptedPrivateKeyExponent;
+      self.publicKeyX = keyPairStoreData.publicKeyX;
+      self.publicKeyY = keyPairStoreData.publicKeyY;
+      self.bitcoinAddress = keyPairStoreData.bitcoinAddress;
+
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   KeyPair.prototype.generate = function(password, hollaback) {
     var self = this;
@@ -20,6 +53,8 @@
       self.publicKeyX = keyPair.publicKeyX;
       self.publicKeyY = keyPair.publicKeyY;
       self.bitcoinAddress = keyPair.bitcoinAddress;
+
+      self.saveToStore();
 
       if (typeof hollaback === "function") {
         hollaback(keyPair);
